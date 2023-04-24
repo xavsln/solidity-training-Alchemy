e10483e253b9295ed8a799a3138d11414156200d
xavsln <test@test.com>
2023-04-24 16:38:04 +0200

    Inject ERC721 from OpenZeppelin

--- contracts/NftVotingSystem.sol
+++ contracts/NftVotingSystem.sol
@@ -1,7 +1,32 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
-pragma solidity 0.8.17;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+// Extension of ERC721 to support voting and delegation as implemented by Votes, where each individual NFT counts as 1 vote unit.
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NftVotingSystem {
+contract MyVotingToken is ERC721, Ownable, EIP712, ERC721Votes {
+    using Counters for Counters.Counter;
 
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("MyVotingToken", "MVT") EIP712("MyVotingToken", "1") {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Votes)
+    {
+        super._afterTokenTransfer(from, to, tokenId, batchSize);
+    }
 }
